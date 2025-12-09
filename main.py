1620f7f1495c6d54cbc614819a77df2bec31a710
Alexandra Sofronie <alexandrateodora.sofronie@gracia.lasalle.cat>
2025-12-09 04:53:58 +0100

    final commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def on_forever():
+    luz = input.light_level()
+    
+    if luz > 200:
+        pass
+    elif luz < 50:
+        pass
+    else:
+        pass
+
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+let temperatura = 0
+let luz = 0
 basic.forever(function () {
-	
+    temperatura = input.temperature()
+    if (temperatura > 22) {
+        basic.showString("CALOR")
+    } else {
+        basic.showString("FRIO")
+    }
+})
+basic.forever(function () {
+    luz = input.lightLevel()
+    if (luz > 200) {
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Prelude), music.PlaybackMode.UntilDone)
+    } else if (luz < 50) {
+        music.play(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.UntilDone)
+    } else {
+        basic.clearScreen()
+    }
 })
